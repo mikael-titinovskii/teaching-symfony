e12fb0cbabe101a469adfbd97ab7eff337f96809
yalti <yaltinets84@gmail.com>
2022-05-14 15:38:03 +0300

    home work Alex

--- alex.php
+++ alex.php
@@ -1,19 +1,31 @@
 <?php
 
 //sort
-//$a = [1, 2, 3, 4];
-//$b = sort($a);
-//print_r($a);
-//echo "\n";
-//
-////rsort
-//$a = [1, 2, 3, 4];
-//$b = rsort($a);
-//print_r($a);
-//echo "\n";
-
-//usort
 $a = [1, 2, 3, 4];
-$b = usort($a);
+$b = sort($a);
 print_r($a);
 echo "\n";
+
+//rsort
+$a = [1, 2, 3, 4];
+rsort($a);
+print_r($a);
+echo "\n";
+
+//usort
+function sortirovka($a, $b){
+    if($a==$b){
+        return 0;
+    }
+    if($a<$b){
+        return -1;
+    }
+    else{
+        return 1;
+    };
+}
+$a = [4, 2, 1, 3];
+usort($a, "sortirovka");
+foreach($a as $key => $value){
+    echo "$key: $value \n";
+}
